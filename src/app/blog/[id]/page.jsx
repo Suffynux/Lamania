647bdd4c9b9b2cb8d839270b423946c02ec26ec0
Sufiyan <suffynux@gmail.com>
2025-04-23 20:08:34 +0500

    Blogs data fetch

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 
+
+const getData = async (id) => {
+  const res = await fetch(`https://dummyjson.com/posts/${id}` , {cache: "no-store"
+  });
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+}
 const BlogPost = async ({ params }) => {
+
+  const data = await getData(params.id)
+  console.log(data.id);
+  
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Heading</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet.
+            {data.body}
           </p>
           <div className={styles.author}>
             <Image
-              src={""}
+              src={"/dummyBLog.jpg"}
               alt=""
               width={40}
               height={40}
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getData = async () => {
+    const res = await fetch("https://dummyjson.com/posts" , {cache: "no-store"
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    const data = await res.json();
+    setPosts(data.posts); // Ensure you access the correct key
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/cat1.jpg"
-            alt="blog"
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>TITLE</h1>
-          <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, quis distinctio temporibus earum blanditiis nam molestiae totam officia quod voluptatum laborum eaque soluta deserunt ducimus dicta, corrupti consequatur? Ad?</p>
-        </div>
-      </div>
-
+      {posts.map((post) => (
+        <Link href={`/blog/${post.id}`} key={post.id}>
+          <div className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image
+                src="/dummyBLog.jpg"
+                alt="blog"
+                fill={true}
+                className={styles.img}
+              />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{post.title}</h1>
+              <p className={styles.desc}>{post.body}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
